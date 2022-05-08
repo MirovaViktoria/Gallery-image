835819c8f68d91e4abd31d54838abbd2a08df134
Vika <mirovik2015@gmail.com>
2022-05-08 18:42:31 +0300

    add fixed

--- js/script.js
+++ js/script.js
@@ -1,18 +1,47 @@
-const url = "https://www.flickr.com/services/rest/?method=flickr.photos.search&per_page=30&api_key=0f15ff623f1198a1f7f52550f8c36057&tags=spring,nature&tag_mode=all&extras=url_m&format=json&nojsoncallback=1"
+const link = "https://www.flickr.com/services/rest/?method=flickr.photos.search&per_page=30&api_key=0f15ff623f1198a1f7f52550f8c36057&tags=spring,nature&tag_mode=all&extras=url_m&format=json&nojsoncallback=1"
 
-async function getData() {
+async function loadData(url) {
     const res = await fetch(url);
     const data = await res.json();
-    console.log(data.photos.photo[0]);
+    var flex = document.getElementById("flex");
+    flex.innerHTML = "";
     for (let i = 0; i < data.photos.photo.length; i++) {
-        var tag = document.createElement("img");
+        var img = document.createElement("img");
+        var div = document.createElement("div");
         var title = document.createElement("div");
-        tag.src = data.photos.photo[i].url_m;
-        var element = document.getElementById("flex");
-        element.appendChild(tag);
+        img.addEventListener("click", function (event) {
+            document.getElementById("image_box").style.display = "block";
+            document.getElementById('img_box').src = event.target.src;
+        });
+        img.src = data.photos.photo[i].url_m;
+        title.innerHTML = data.photos.photo[i].title;
+        div.appendChild(title);
+        flex.appendChild(div);
+        div.appendChild(img);
     }
 
 }
-getData();
+loadData(link);
+image_box.addEventListener("click", function (event) {
+    document.getElementById("image_box").style.display = "none";
+});
 
+const input = document.querySelector("#input");
+input.addEventListener("keyup", function (event) {
+    if (event.key === "Enter") {
+        search();
+    }
+});
 
+const buttom = document.querySelector("#search_button");
+buttom.addEventListener("click", function (event) {
+    search();
+});
+function search() {
+    loadData(link + '&text=' + input.value);
+}
+
+// сперва надо что ?
+//     1 - сделать при нажатии элемент видимым,то есть сделать display = block
+// 2 - надо чтобы определяло картинку по которой нажали и увеличить ее
+// 3 - надо чтобы при втором нажатии картинка закрывалась
